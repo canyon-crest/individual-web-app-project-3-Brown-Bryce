f2114efeb34b1aee5a07bc7578b7579d0ea8cbf8
3-Brown-Bryce <brownb8673@my.sduhsd.net>
2026-03-03 21:55:02 +0000

    Made ccs mostly

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// add javascript here
+document.getElementById("togglePhysics")?.addEventListener("click", () => {
+    document.getElementById("physicsFact").toggleAttribute("hidden");
+})
+
+document.getElementById("effectBtn")?.addEventListener("click", () => {
+    document.getElementById("physicsRainbow").toggleAttribute("hidden")
+})
